77e51984790720cb0d904bc61eaee0feb36ceb1c
r4nd0lph <r11345roma@yandex.ru>
2024-11-22 18:58:35 +0300

    Init project

---
+++ Assets/Scripts/Time/TimeManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace Time
+{
+    public class TimeManager : MonoBehaviour
+    {
+        void Start()
+        {
+        }
+
+        void Update()
+        {
+        }
+    }
+}
