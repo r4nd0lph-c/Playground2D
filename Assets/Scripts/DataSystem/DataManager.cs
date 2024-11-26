3b83763f118cea745b1990a7a3b5e96bddd487d8
r4nd0lph <r11345roma@yandex.ru>
2024-11-27 01:21:33 +0300

    Refactor

---
+++ Assets/Scripts/DataSystem/DataManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace DataSystem
+{
+    public class DataManager : MonoBehaviour
+    {
+        public static DataManager Instance;
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TimeSystem/TimeManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace TimeSystem
+{
+    public class TimeManager : MonoBehaviour
+    {
+        public static TimeManager Instance;
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
